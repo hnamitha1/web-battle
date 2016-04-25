5b5373bfccb55c781de06d726c6fdd93be90ad1f
MariaRomero <fantasysystems@gmail.com>
2016-04-25 17:58:23 +0100

    challenge 12

--- app.rb
+++ app.rb
@@ -8,7 +8,14 @@ get '/secret' do
 	"it's me again"
 end
 
-get '/cat' do
+get '/random-cat' do
 	@names = ["Amigo", "Maria", "Pedro"].sample
+	erb(:index)
+end
+
+get '/named-cat' do
+	p params
+	@color = params[:color]
+	@names = params[:name]
 	erb(:index)
 end

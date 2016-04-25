5257d9b9726aa193e4cd50377642b6832b1a627d
hnamitha1 <namitha.h@gmail.com>
2016-04-25 16:49:10 +0100

    Challenge 8 :cute cat

--- app.rb
+++ app.rb
@@ -9,6 +9,12 @@ set :session_secret, 'super secret'
     "This is a secret page"
   end
 
+  get '/cat' do
+   "<div>
+    <img style='border: 3px dashed red' src='http://bit.ly/1eze8aE'>
+   </div>"
+  end
+
   get '/demo2' do
     "This is a new demo"
   end
